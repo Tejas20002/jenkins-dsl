482a54cbaa4fdf282e329a7b08e33b7c9e047187
tjhirani <52062174+Tejas20002@users.noreply.github.com>
2022-05-24 13:13:54 +0530

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,4 +1,16 @@
 job('demo') {
+    scm{
+        git('git://https://github.com/Tejas20002/jenkins-dsl.git'){
+            node -> node / gitConfigName('tjhirani')
+            node / gitCongifEmail('tejashirani@gmail.com')
+        }
+    }
+    triggers{
+        scm('H/S * * * *')
+    }
+    wrappers{
+        nodejs('nodejs')
+    }
     steps {
         shell('echo Hello World!')
     }
